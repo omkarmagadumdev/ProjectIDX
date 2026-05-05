3d598d857bcdc8e505425d2493120f01cf92fe39
Omkar Magadum <omkar.magadum.dev@gmail.com>
2026-05-05 16:14:22 +0000

    Refactor server setup and add ping endpoint with controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import  { PORT  } from './config/serverConfig.js'
-
+import apiRouter from './routes/index.js'
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
-app.use(cors);
-
-
-app.listen(PORT,()=>{
-    console.log(`server is running on the following port ${PORT} `);
-    
-})
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get('/ping',(req,res)=>{
     return res.json({ message:"pong" })
+})
+
+app.use('/api',apiRouter)
+
+app.listen(PORT,()=>{
+    console.log(`server is running on the following port ${PORT} `);
 })

75a08ff83f81ff619ca85e8714b7d15f42f76887
Omkar Magadum <omkar.magadum.dev@gmail.com>
2026-05-06 14:41:46 +0000

    Refactor useCreateProject hook and remove unnecessary README file

--- frontend/src/hooks/apis/mutations/useCreateProject.js
+++ frontend/src/hooks/apis/mutations/useCreateProject.js
@@ -14,6 +14,7 @@ const { mutateAsync,isPending,isSuccess,isError } = useMutation({
             }  
         })
 
+        
         return {
             createProjectMutation:mutateAsync,
             isPending,

6c39d0640bee1cc87ed25c19451422465a0e0f02
Omkar Magadum <omkar.magadum.dev@gmail.com>
2026-05-13 15:36:52 +0000

    Add socket.io integration for real-time communication; update server setup in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import  { PORT  } from './config/serverConfig.js'
 import apiRouter from './routes/index.js'
+import { Server } from 'socket.io';
+import { createServer, get } from 'node:http'
 const app = express();
+
+const server = createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin:'*',
+        methods:['GET','POST']
+    }
+});
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,8 +23,13 @@ app.get('/ping',(req,res)=>{
     return res.json({ message:"pong" })
 })
 
+io.on('connection',(socket)=>{
+    console.log("a user connected");
+    
+})
+
 app.use('/api',apiRouter)
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log(`server is running on the following port ${PORT} `);
 })

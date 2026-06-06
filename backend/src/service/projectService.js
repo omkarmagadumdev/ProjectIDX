3922d2b8ae628e8dfe012bb51e192a980f20c58c
Omkar Magadum <omkar.magadum.dev@gmail.com>
2026-06-06 10:01:08 -0600

    await project scaffold so container starts after files exist

--- backend/src/service/projectService.js
+++ backend/src/service/projectService.js
@@ -14,9 +14,12 @@ export const createProjectService = async () => {
 
     // Run project scaffolding command in background so HTTP request doesn't hang.
     if (REACT_PROJECT_COMMOND) {
-        execPromisified(REACT_PROJECT_COMMOND, { cwd: `./projects/${projectId}` })
-            .then((r) => console.log('scaffold finished', r))
-            .catch((err) => console.error('scaffold error', err));
+        try {
+            const r = await execPromisified(REACT_PROJECT_COMMOND, { cwd: `./projects/${projectId}` });
+            console.log('scaffold finished', r)
+        } catch (err) {
+            console.error('scaffold error', err)
+        }
     }
 
     return projectId;

865c8a7d717b2881f1b77e8423c7725b380a58e5
Omkar Magadum <omkar.magadum.dev@gmail.com>
2026-06-06 09:54:41 -0600

    trigger container creation when a new project is created

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -1,10 +1,13 @@
 import { createProjectService, getProgectTreeService } from '../service/projectService.js';
+import { handleCreateContainer } from '../containers/handleCreateContainers.js';
 
 export const createProjectConroller = async (req, res) => {
   console.log('createProjectConroller: received request')
   try {
     const projectId = await createProjectService()
     console.log('createProjectConroller: created', projectId)
+    // Trigger container creation for the new project (fire-and-forget)
+    try{ handleCreateContainer(projectId).then(()=>console.log('container create triggered')) }catch(e){ console.warn('failed to trigger container create', e)}
     return res.json({ message: 'Project created', data: projectId })
   } catch (err) {
     console.error('createProjectConroller error', err)
